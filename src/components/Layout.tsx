4797b48a2f26244574aa954d5333b608fad14539
nikiskene <ns@iacy.com>
2026-09-20 17:30:30 +0200

    style: soften Samly sidebar background

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -100,7 +100,7 @@ export function Layout({ children }: LayoutProps) {
         <aside
           className={`${
             sidebarOpen ? 'translate-x-0' : '-translate-x-full'
-          } fixed inset-y-0 left-0 z-50 w-64 bg-gray-900 text-white transition-transform duration-300 ease-in-out lg:translate-x-0 lg:static lg:inset-0`}
+          } fixed inset-y-0 left-0 z-50 w-64 bg-black/80 text-white backdrop-blur-sm transition-transform duration-300 ease-in-out lg:translate-x-0 lg:static lg:inset-0`}
         >
           <div className="flex flex-col h-full">
             <div className="flex items-center justify-between p-4 border-b border-gray-800">
